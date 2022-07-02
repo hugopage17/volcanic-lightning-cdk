25fb3aa69d7688f277da4d37983503d80c3df7e6
hugopage17 <hugopage0017@gmail.com>
2022-07-02 18:47:00 +1200

    working unit test

--- test/lightning-poller.test.ts
+++ test/lightning-poller.test.ts
@@ -4,37 +4,35 @@ import lightningPoller from "../api/lightning-poller";
 
 
 describe('Lightning Poller response', () => {
-    it('Response should match the schema', () => {
-        lightningPoller().then((res) => {
-            const featureSchema: ZodSchema<Feature> = z.object({
-                type: z.literal('Feature'),
-                geometry: z.object({
-                    type: z.literal('Point'),
-                    coordinates: z.number().array() as any as ZodType<[number, number]>
-                }).required().strict(),
-                properties: z.object({
-                    number: z.string(),
-                    name: z.string(),
-                    area: z.string(),
-                    twentyKmStrikes: z.number(),
-                    hundredKmStrikes: z.number(),
-                    volcanoType: z.string(),
-                    severity: z.string(),
-                }).required().strict()
-            }).required().strict()
+    it('Response should match the schema', async () => {
+        const res = await lightningPoller()
+        const featureSchema = z.object<any>({
+            type: z.literal('Feature') as any as ZodType<string>,
+            geometry: z.object<any>({
+                type: z.literal('Point') as any as ZodType<string>,
+                coordinates: z.number().array() as any as ZodType<[number, number]>
+            }) as any as ZodSchema<Feature['geometry']>,
+            properties: z.object<any>({
+                name: z.string(),
+                area: z.string(),
+                twentyKmStrikes: z.number(),
+                hundredKmStrikes: z.number(),
+                volcanoType: z.string(),
+                severity: z.string(),
+            }) as any as ZodSchema<Feature['properties']>,
+        }) as any as ZodSchema<Feature>
 
-            const schema: ZodSchema<GeoJSON> = z.object({
-                type: z.literal('FeatureCollection'),
-                timestamp: z.string(),
-                features: z.array(featureSchema)
-            }).required().strict()
-
-            const validator = schema.safeParse(res);
-            expect(validator.success).toBe(true);
-            expect(res.timestamp).toBeDefined()
-            if (res.features.length) {
-                expect(res.features[0].properties.name).toBeDefined()
-            }
-        });
+        const schema = z.object<any>({
+            type: z.literal('FeatureCollection') as any as ZodType<string>,
+            timestamp: z.string(),
+            features: featureSchema.array()
+        }) as any as ZodSchema<GeoJSON>
+        
+        const validator = schema.safeParse(res);
+        expect(validator.success).toBe(true);
+        expect(res.timestamp).toBeDefined()
+        if (res.features.length) {
+            expect(res.features[0].properties.name).toBeDefined()
+        }
     });
 });
